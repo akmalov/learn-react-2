8c6e0d4ef0ec0aabfa35fdc0f75a534ad160d2b5
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-26 13:36:18 +0300

    add youtube api key and search api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = ' AIzaSyAhOi5N-FdyluS0oQqoyw-Dfb5YxB5RcxM';
+
 class App extends React.Component {
     render() {
         return <div>Hi</div>;
